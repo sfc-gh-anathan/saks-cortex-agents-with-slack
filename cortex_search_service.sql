9a99a58e0963e3d9ae25a5462c6d6c3709911a7a
Adam Nathan <adam.nathan@snowflake.com>
2025-06-11 11:08:12 -0400

    swapped out search service for info_search changed the chunking size and included a larger WH for the initial chunking

--- cortex_search_service.sql
+++ cortex_search_service.sql
@@ -6,6 +6,9 @@
 USE DASH_DB.DASH_SCHEMA;
 USE WAREHOUSE DASH_S;
 
+create warehouse TEMP_LARGE warehouse_size = 'large';
+use warehouse TEMP_LARGE;
+
 -- Pull the data from a stage into a table. 
 -- Note the use of cortex.parse_document function to parse the PDF files.
 create or replace table parse_pdfs as 
@@ -17,7 +20,7 @@ select relative_path, SNOWFLAKE.CORTEX.PARSE_DOCUMENT(@DASH_DB.DASH_SCHEMA.DASH_
 create or replace table parsed_pdfs as (
     with tmp_parsed as (select
         relative_path,
-        SNOWFLAKE.CORTEX.SPLIT_TEXT_RECURSIVE_CHARACTER(TO_VARIANT(data):content, 'MARKDOWN', 300, 100) AS chunks
+        SNOWFLAKE.CORTEX.SPLIT_TEXT_RECURSIVE_CHARACTER(TO_VARIANT(data):content, 'MARKDOWN', 500, 150) AS chunks
     from parse_pdfs where TO_VARIANT(data):content is not null)
     select
         TO_VARCHAR(c.value) as PAGE_CONTENT,
@@ -31,7 +34,7 @@ create or replace table parsed_pdfs as (
 -- This service will allow you to search through the parsed content.
 -- This search service is set to target a lag of 1 hour, meaning it will update the search index every hour.
 -- It will be called by the Cortex Agent via the Cortex Search Service API (routed from the Cortex Agent to the Snowflake Cortex Search Service).
-create or replace CORTEX SEARCH SERVICE DASH_DB.DASH_SCHEMA.VEHICLES_INFO
+create or replace CORTEX SEARCH SERVICE DASH_DB.DASH_SCHEMA.info_search
 ON PAGE_CONTENT
 WAREHOUSE = DASH_S
 TARGET_LAG = '1 hour'
@@ -39,3 +42,5 @@ AS (
     SELECT '' AS PAGE_URL, PAGE_CONTENT, TITLE, RELATIVE_PATH
     FROM parsed_pdfs
 );
+
+drop warehouse TEMP_LARGE;
