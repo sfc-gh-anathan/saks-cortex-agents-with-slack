93e84acddfdb8ad79d57dfe1627f41b1c1cff12f
Adam Nathan <adam.nathan@snowflake.com>
2025-06-11 09:09:29 -0400

    this file now uses the built in .env variables and load_dotenv() to access them. the file should now be ready to test the slack connection as it with no configuration other than setting the APP and BOT env information

---
+++ test-slack-connection.py
@@ -0,0 +1,25 @@
+import os
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+from dotenv import load_dotenv
+
+load_dotenv()
+SLACK_APP_TOKEN = os.getenv("SLACK_APP_TOKEN")
+SLACK_BOT_TOKEN = os.getenv("SLACK_BOT_TOKEN")
+
+# Initializes your app with your bot token and socket mode handler
+# app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+app = App(token=SLACK_BOT_TOKEN)
+
+# Listens to incoming messages that contain "hello"
+# To learn available listener arguments,
+# visit https://tools.slack.dev/bolt-python/api-docs/slack_bolt/kwargs_injection/args.html
+@app.message("hello")
+def message_hello(message, say):
+    # say() sends a message to the channel where the event was triggered
+    say(f"Hey there <@{message['user']}>!")
+
+# Start your app
+if __name__ == "__main__":
+    # SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
+    SocketModeHandler(app, SLACK_APP_TOKEN).start()    
